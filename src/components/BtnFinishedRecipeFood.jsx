4c456da9352e06229d871e68a97c30e4ca83aa63
brunoro7 <bruno.ro7c@gmail.com>
2022-05-11 03:15:35 -0300

    requisito 53 ok.

--- src/components/BtnFinishedRecipeFood.jsx
+++ src/components/BtnFinishedRecipeFood.jsx
@@ -1,28 +1,22 @@
-// import PropTypes from 'prop-types';
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../pages/DrinkDetails.css';
 import '../pages/Recomendation.css';
 
 class BtnFinishedRecipeFood extends React.Component {
   render() {
-    // const { idDrinkRecipe } = this.props;
     return (
-      // <Link to={ `/drinks/${idDrinkRecipe}/in-progress` }>
-      <button
-        type="button"
-        data-testid="finish-recipe-btn"
-        className="btnInitRecipeFood"
-      >
-        Finish Recipe
-      </button>
-      // </Link>
+      <Link to="/done-recipes">
+        <button
+          type="button"
+          data-testid="finish-recipe-btn"
+          className="btnInitRecipeFood"
+        >
+          Finish Recipe
+        </button>
+      </Link>
     );
   }
 }
-
-BtnFinishedRecipeFood.propTypes = {
-  // idDrinkRecipe: PropTypes.string.isRequired,
-};
 
 export default BtnFinishedRecipeFood;
